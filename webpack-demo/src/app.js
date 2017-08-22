be562867259998629881336863fe875e37305450
Kasia <katarzyna.m1405@gmail.com>
2017-08-22 10:58:30 +0200

    add webpack to the project

---
+++ webpack-demo/src/app.js
@@ -0,0 +1,17 @@
+import { groupBy } from 'lodash/collection'
+import people from './people'
+import './style.scss'
+import codeURL from './code.png'
+
+const img = document.createElement('img');
+img.src = codeURL;
+img.style.backgroundColor = "#2B3A42";
+img.style.padding = "20px";
+img.width = 32;
+document.body.appendChild(img);
+
+
+const managerGroups = groupBy(people, 'manager');
+
+const root = document.querySelector('#root');
+root.innerHTML = `<pre>${JSON.stringify(managerGroups, null, 2)}</pre>`
---
+++ webpack-demo/src/people.js
@@ -0,0 +1,19 @@
+const people = [{
+        manager: 'Jen',
+        name: 'Bob'
+    },
+    {
+        manager: 'Jen',
+        name: 'Sue'
+    },
+    {
+        manager: 'Bob',
+        name: 'Shirley'
+    },
+    {
+        manager: 'Bob',
+        name: 'Terrence'
+    }
+]
+
+export default people;
