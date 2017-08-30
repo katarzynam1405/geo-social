890f873cec3483e8c4da017e638e5605c1a50270
Katarzyna Malek <kmal@softserveinc.com>
2017-08-30 15:38:08 +0200

    flash message in progress

--- src/app.js
+++ src/app.js
@@ -3,18 +3,25 @@ import './style.scss';
 import './modules/button/button.scss';
 import button from './modules/button/button';
 import geolocation from './modules/geolocation/geolocation';
+import flashAlert from './modules/flashAlert/flashAlert';
 
 
 function geo(Position) {
     console.log(Position);
 }
+function show(message){
+    console.log(message)
+}
 
 function error(PositionError) {
     console.log(PositionError, PositionError.message);
-}
+    show("User denied");
+} 
+
 
 function initGeolocation() {
     geolocation(geo, error);
 };
 
 button(initGeolocation);
+flashAlert(show);
---
+++ src/modules/flashAlert/flashAlert.js
@@ -0,0 +1,5 @@
+export default function flashAlert(show){
+    setTimeout( function show(message){
+        alert("z flashAlert")
+    }, 3000);
+}
