fcc33031d2c59eb93da510f74f56e1c97d37b09b
Kasia <katarzyna.m1405@gmail.com>
2017-09-05 23:06:18 +0200

    add API module, fatch images from instagram profile

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import { groupBy } from 'lodash/collection'
 
+import API from './modules/API/API';
 import alert from './modules/alert/alert';
 import button from './modules/button/button';
 import geolocation from './modules/geolocation/geolocation';
@@ -7,18 +8,19 @@ import geolocation from './modules/geolocation/geolocation';
 import './style.scss';
 import './modules/button/button.scss';
 
-function onGeolocationSuccess(position){
+function onGeolocationSuccess(position) {
     console.log(position);
 }
 
-function onGeolocationError(positionError){
+function onGeolocationError(positionError) {
     console.log(positionError, positionError.message);
     alert(positionError.message, "warning", 5000);
 }
 
-function onClickHandler(){
+function onClickHandler() {
     geolocation().then(onGeolocationSuccess).catch(onGeolocationError);
 };
 
 button(onClickHandler);
 
+API();
