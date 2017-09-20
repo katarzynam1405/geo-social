8829fd9909f69019589832b80656623a7ae6cc2c
Katarzyna Malek <kmal@softserveinc.com>
2017-09-20 13:27:12 +0200

    photos render only after click on 'show more' button

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ import './modules/button/button.scss';
 
 function onGeolocationSuccess(position) {
     console.log(position.coords.latitude, position.coords.longitude);
-    API(position.coords.latitude, position.coords.longitude).then(parseJson).then(onShowMoreClickHandler).catch(e => alert(e, "warning", 100000));
+    API(position.coords.latitude, position.coords.longitude).then(parseJson).catch(e => alert(e, "warning", 100000));
 }
 
 function onGeolocationError(positionError) {
@@ -27,19 +27,20 @@ function onClickHandler() {
 let init=0;
 let added=20;
 
+const store = {}; 
 function parseJson(data){  
-  const dane = data;
-  console.log(dane);
-  return dane;
+   return store.data = data
 }
 
-function onShowMoreClickHandler(dane){
+console.log(store)
+
+
+function onShowMoreClickHandler(data){
     console.log(init+=20);
     console.log(added+=20);
-    let images = dane.photos.photo.slice(init, added);
+    let images = store.data.photos.photo.slice(init, added);
     images.map(getImgUrl).map(renderImage);
 }
 
 button(onClickHandler);
 imagesButton(onShowMoreClickHandler);
-
